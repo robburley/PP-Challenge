57fbbb89c55f7c4a4a4fb2525f8bd3c311586936
Rob Burley <rob.burley@boohoo.com>
2022-02-12 20:27:59 +0000

    Add scheduler for command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('ticket:generate')->everyMinute();
+        $schedule->command('ticket:process')->everyFiveMinutes();
     }
 
     /**
